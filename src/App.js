9a6373701b5f87a39f21cde999a0a9abce20d686
nimrod aviam <nimrod.a6@gmail.com>
2023-12-29 21:47:04 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+// npx create-react-app redux  
+// npm install @reduxjs/toolkit react-redux
+
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTopping, toggleGluten } from './pizzaSlice';
 
 function App() {
+  const pizza = useSelector(state => state.pizza)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pizza</h1>
+        {pizza.gluten ? <>gluten</> : <>gluten free</>}
+        {pizza.topping.map(topping => (
+          <div key={topping}>{topping}</div>
+        ))}
+
+        <button onClick={() => dispatch(addTopping('pepperoni'))}>Add Pepperoni</button>
+        <button onClick={() => dispatch(addTopping('mashroom'))}>Add mashroom</button>
+        <button onClick={() => dispatch(addTopping('onion'))}>Add onion</button>
+        <button onClick={() => dispatch(toggleGluten())}>toggle gluten</button>
       </header>
     </div>
   );
